68038757c6727c97cddd67cda3fd8ebfe1337aa9
Patrick Gerken <do3cc@patrick-gerken.de>
2025-03-04 22:38:08 +0100

    refactor(fish): Give up on maintaining scripts
    Instead, I use a package manager now

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,21 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
-    pyenv init - | source
     direnv hook fish | source
-    fzf --fish | source
-    fish_ssh_agent
+    uv generate-shell-completion fish | source
+    fish_add_path "/home/do3cc/.local/share/../bin"
     export XDG_DATA_HOME=$HOME/.local/share
     export XDG_CONFIG_HOME=$HOME/.config
     export XDG_STATE_HOME=$HOME/.local/state
     export XDG_CACHE_HOME=$HOME/.cache
     export XINITRC="$XDG_CONFIG_HOME"/X11/xinitrc
     export SQLITE_HISTORY="$XDG_CACHE_HOME"/sqlite_history
-    export PYENV_ROOT="$XDG_DATA_HOME"/pyenv
     export NVM_DIR="$XDG_DATA_HOME"/nvm
     export LESSHISTFILE="$XDG_STATE_HOME"/less/history
     export ANSIBLE_HOME="$XDG_DATA_HOME"/ansible
+    export NODE_VERSIONS="$XDG_DATA_HOME/nvm"
+    export NODE_VERSION_PREFIX=""
+    nvm use lts
 end
+
+# uv
