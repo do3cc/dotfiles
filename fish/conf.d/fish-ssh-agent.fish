a16f9f1f463d56f16fb947c73973e916c7e3eed7
Patrick Gerken <do3cc@patrick-gerken.de>
2026-09-26 01:16:12 +0200

    fish updated some plugins we have in our git

--- fish/conf.d/fish-ssh-agent.fish
+++ fish/conf.d/fish-ssh-agent.fish
@@ -5,8 +5,3 @@ end
 if not __ssh_agent_is_started
     __ssh_agent_start
 end
-
-# Auto-load default SSH key (symlink created by init script)
-if test -f $HOME/.ssh/id_ed25519_default
-    ssh-add $HOME/.ssh/id_ed25519_default 2>/dev/null
-end
